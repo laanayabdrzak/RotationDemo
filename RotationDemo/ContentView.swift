a2f4148b179d6a91eaa1e4541b8ab7d5eea83260
Abderrazak LAANAYA <laanayabdrzak@gmail.com>
2024-04-24 12:46:39 +0100

    Initial Commit

---
+++ RotationDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RotationDemo
+//
+//  Created by LAANAYA Abderrazak on 24/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RotationDemo/RotationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotationDemoApp.swift
+//  RotationDemo
+//
+//  Created by LAANAYA Abderrazak on 24/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct RotationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
